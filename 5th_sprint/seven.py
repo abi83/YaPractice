351fbdbb9de29259913328a168cab921d98e5e9e
Abi88 <vladimir.kromm@yandex.ru>
2020-10-22 23:49:15 +0700

    2nd variant of tests (seven)

--- 5th_sprint/seven.py
+++ 5th_sprint/seven.py
@@ -27,6 +27,9 @@ class Contact:
     def print_contact(self) -> None:
         print(self.show_contact())
 
+    def __dir__(self):
+        return ['age_define', 'programmer_define']
+
 
 # Протестируйте методы programmer_define() и age_define() класса Contact.
 # Тестировать нужно все состояния: метод programmer_define() должен по очереди
@@ -45,15 +48,66 @@ test_values = {
     (1981, False): ['Молодой', 'Нормальный'],
 }
 
+# первый вариант
 for key in test_values:
-    loop_man = Contact('Testman',*key)
+    loop_man = Contact('Test Man', *key)
 
     test_methods = [
         loop_man.age_define,
         loop_man.programmer_define,
     ]
-    print('Testing', loop_man.show_contact(), 'OK!')
 
     for i in range(len(test_methods)):
-        print('    Testing', test_methods[i].__name__, 'OK!')
-        assert test_methods[i]() == test_values[key][i], f'Method {i} is down'
+        assert test_methods[i]() == test_values[key][i],\
+            f'Method {test_methods[i].__name__} is down'
+
+
+# второй вариант
+test_methods = Contact('friend', 1900, False).__dir__()
+# создаем список названий методов класса. Методо __dir__ переопределен
+test_values = {
+    (1945, True): {'age_define': 'Старейшина', 'programmer_define': 'Программист'},
+    (1979, True): {'age_define': 'Олдскул', 'programmer_define': 'Программист'},
+    (1980, True): {'age_define': 'Молодой', 'programmer_define': 'Программист'},
+    (1981, True): {'age_define': 'Молодой', 'programmer_define': 'Программист'},
+    (1945, False): {'age_define': 'Старейшина', 'programmer_define': 'Нормальный'},
+    (1979, False): {'age_define': 'Олдскул', 'programmer_define': 'Нормальный'},
+    (1980, False): {'age_define': 'Молодой', 'programmer_define': 'Нормальный'},
+    (1981, False): {'age_define': 'Молодой', 'programmer_define': 'Нормальный'},
+}
+
+for key in test_values:
+    loop_man = Contact('Test Man', *key)
+    for method in test_methods:
+        # вызываем метод по его названию
+        assert loop_man.__getattribute__(method)() == test_values[key][method],\
+            f'Method {method} is down'
+
+
+
+
+
+
+
+
+
+
+
+
+assert Contact('friend', 1945, True).age_define() == 'Старейшина', ' (('
+assert Contact('friend', 1945, True).programmer_define() == 'Программист', ' (('
+
+assert Contact('friend', 1979, True).age_define() == 'Олдскул', ' (('
+assert Contact('friend', 1979, True).programmer_define() ==  'Программист', ' (('
+
+
+
+# (1980, True): ['Молодой', 'Программист'],
+# (1981, True): ['Молодой', 'Программист'],
+# (1945, False): ['Старейшина', 'Нормальный'],
+# (1979, False): ['Олдскул', 'Нормальный'],
+
+
+assert Contact('friend', 1980, False).age_define() == 'Молодой', ' (('
+assert Contact('friend', 1980, False).programmer_define() == 'Нормальный', ' (('
+# (1981, False): ['Молодой', 'Нормальный'],

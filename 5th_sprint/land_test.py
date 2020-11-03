6e1b6e8c974bde27589b74c28f3715be6cf2da5e
Abi88 <vladimir.kromm@yandex.ru>
2020-11-03 12:45:55 +0700

    land and point exercise

--- 5th_sprint/land_test.py
+++ 5th_sprint/land_test.py
@@ -1,5 +1,5 @@
 """
-There is 4 points (x,y) forming the land. And one random point.
+There are 4 points (x,y) forming the land. And one random point.
 Be sure that the point is inside the land or not.
 """
 import unittest
@@ -12,8 +12,25 @@ def func_from_line(a: tuple, b: tuple) -> Callable[[int], int]:
     """
     def f(x):
         """ the line function y = f(x)"""
+        # try:
         return a[1] + (b[1]-a[1])/(b[0]-a[0])*x - (b[1]-a[1])/(b[0]-a[0])*a[0]
     return f
+
+
+def return_left_point(points_list: List[tuple]) -> tuple:
+    """
+    Returns the point, tuple such as (x,y) from points_list with minimal
+    x coordinate. When there are two points it returns the bottom left point
+    """
+    return min(points_list)
+
+
+def return_right_point(points_list: List[tuple]) -> tuple:
+    """
+    Returns the point, tuple such as (x,y) from points_list with maximal
+    x coordinate. When there are two points it returns the upper right point
+    """
+    return max(points_list)
 
 
 def return_point(point_list: List[tuple], min_max: str, x_y: int) -> tuple:
@@ -30,6 +47,8 @@ def return_point(point_list: List[tuple], min_max: str, x_y: int) -> tuple:
         for i in range(1, len(point_list)):  # starting from second element
             if point_list[i][x_y] < min_point[x_y]:
                 min_point = point_list[i]
+            elif point_list[i][x_y] == min_point[x_y]:
+                raise ValueError('Tho points returned')
         return min_point
     elif min_max == 'max':
         max_point = point_list[0]  # starting from first element
@@ -47,7 +66,7 @@ def left_check(land: List[tuple], point: tuple) -> bool:
     Building two rays from left point to upper and bottom points
     Checking that the point is between the rays
     """
-    min_x = return_point(land, 'min', 0)  # left point
+    min_x = return_left_point(land)
     min_y = return_point(land, 'min', 1)  # upper point
     max_y = return_point(land, 'max', 1)  # bottom point
 
@@ -64,7 +83,7 @@ def right_check(land: List[tuple], point: tuple) -> bool:
     Building two rays from right point to upper and bottom points
     Checking that the point is between the rays
     """
-    max_x = return_point(land, 'max', 0)  # right point
+    max_x = return_right_point(land)
     min_y = return_point(land, 'min', 1)  # upper point
     max_y = return_point(land, 'max', 1)  # bottom point
 
@@ -74,7 +93,23 @@ def right_check(land: List[tuple], point: tuple) -> bool:
     return right_high_line(point[0]) >= point[1] >= right_bottom_line(point[0])
 
 
+def check(land: List[tuple], point: tuple) -> bool:
+    left_point = return_left_point(land, point)
+    pass
+
+def check_data(land: List[tuple], point: tuple) -> bool:
+    if len(land) != 4:
+        raise TypeError('Given data is not valid')
+    if len(point) != 2 or type(point) != tuple:
+        raise TypeError('Given data is not valid')
+    for tuple_item in land:
+        if len(tuple_item) != 2 or type(tuple_item) != tuple:
+            raise TypeError('Given data is not valid')
+    return True
+
+
 def run(land: List[tuple], point: tuple) -> bool:
+    check_data(land, point)
     return left_check(land, point) and right_check(land, point)
 
 
@@ -108,6 +143,25 @@ class TestChecks(unittest.TestCase):
         result = True
         self.assertEqual(call, result,
                          'Doesnt work with no upper and bottom point quadrant')
+
+    def test_no_left_and_right_points(self):
+        call = run([(100, 100), (100, 200), (200, 150), (200, 250)], (150, 175))
+        result = True
+        self.assertEqual(call, result,
+                         'Doesnt work with no upper and bottom point quadrant')
+
+
+    def test_non_quadrant_points(self):
+        self.assertRaises(ValueError,
+                          run,
+                          [(100, 100), (100, 200), (100, 300), (200, 200)],
+                          (150, 150))
+
+    def test_check_data_function(self):
+        self.assertRaises(TypeError,
+                          run,
+                          [(100, 100), (200, 200), (300, 100), (200, 100)],
+                          (5, 2, 1))
 
 
 if __name__ == '__main__':

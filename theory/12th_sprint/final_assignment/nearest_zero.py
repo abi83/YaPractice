53939b50854272c6a518150bd9a89ae6d5e29a7f
Abi88 <vladimir.kromm@yandex.ru>
2021-02-05 15:14:21 +0700

    12th assignment to review

--- theory/12th_sprint/final_assignment/nearest_zero.py
+++ theory/12th_sprint/final_assignment/nearest_zero.py
@@ -1,18 +1,4 @@
-"""
-Улица, на которой хочет жить Тимофей, имеет длину n, то есть состоит из n
-одинаковых идущих подряд участков. На каждом участке либо уже построен дом,
-либо участок пустой. Тимофей ищет место для строительства своего дома. Тимофей
-очень общителен и не хочет жить далеко от знакомых, которых у него вся улица.
-Чтобы оптимально выбрать место для строительства, он хочет для каждого участка
-знать расстояние до ближайшего пустого участка. (Для пустого участка эта
-величина будет равна нулю — расстояние до самого себя.)
-
-Ваша задача — помочь Тимофею посчитать искомые расстояния. Для этого у вас есть
-карта улицы. Дома в городе Тимофея нумеровались в том порядке, в котором
-троились, поэтому их номера на карте никак не упорядочены. Пустые участки
-обозначены нулями.
-"""
-
+# success try id: 47973669
 
 def first_iteration(street):
     distance = 'unknown'
@@ -24,7 +10,7 @@ def first_iteration(street):
             yield distance
         try:
             distance += 1
-        except TypeError:
+        except TypeError:  # distance = 'unknown'
             pass
 
 
@@ -53,11 +39,11 @@ def back_iteration(street, n):
 def main():
     with open('input.txt') as file:
         n = int(file.readline().strip())
-        street = (int(x) for x in file.readline().strip().split())
+        street = (int(symbol) for symbol in file.readline().strip().split())
 
-    output1 = first_iteration(street)
-    output2 = back_iteration(list(output1), n)
-    for element in output2:
+    left_side_generator = first_iteration(street)
+    output = back_iteration(list(left_side_generator), n)
+    for element in output:
         print(element, end=' ')
 
 

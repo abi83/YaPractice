a767e323835c5dc28ae151d23ceebc48bedb002a
Abi88 <vladimir.kromm@yandex.ru>
2021-02-17 15:44:15 +0700

    HW12 to third review

--- theory/12th_sprint/final_assignment/smart_hands.py
+++ theory/12th_sprint/final_assignment/smart_hands.py
@@ -16,26 +16,10 @@ def console_input():
     return int(input()), input() + input() + input() + input()
 
 
-def console_output(answer: [int, str]):
-    print(answer)
-
-
-def file_input():
-    with open('input.txt') as file:
-        power = int(file.readline().strip())
-        data = file.read().strip().replace('\n', '')
-    return power, data
-
-
-def file_output(answer: [int, str]):
-    with open('output.txt', 'w') as file:
-        file.write(str(answer))
-
-
 def main():
     power, data = console_input()
     points = key_trainer(data, power)
-    console_output(points)
+    print(points)
 
 
 if __name__ == '__main__':

2fe29c15737b0ecc306bd6e3160e70d137d061ab
Abi88 <vladimir.kromm@yandex.ru>
2021-02-26 21:36:20 +0700

    js algoritms

--- JS course/questions.js
+++ JS course/questions.js
@@ -21,4 +21,5 @@ let beagle = new Dog();
 // Есть ли шанс узнать что у меня - объект или массив
 // Вопрос 5. Как дебажить ???
 // Вопрос 6. Что такое "use strict";
-// Вопрос 7. Кодестайл.
+// Вопрос 7. Кодестайл. на примере:
+// https://github.com/abi83/YaPractice/blob/master/JS%20course/Projects/cash_register.js

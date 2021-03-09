3c0615ddc64b467ad363a75d83e999413d6af5ab
Abi88 <vladimir.kromm@yandex.ru>
2021-03-09 18:23:01 +0700

    broken array partly

--- theory/14th_sprint/A_broken_array.py
+++ theory/14th_sprint/A_broken_array.py
@@ -1,30 +1,48 @@
-def find_in_partially_sorted_array(array, required):
-    left_index, right_index = 0, len(array)
-    median = array[left_index]
-    middle = array[(right_index - left_index) // 2]
+def find_in_partially_sorted_array(array, required, from_index, to_index):
+    a = array[from_index]
+    middle_index = (from_index + to_index) // 2
+    median = array[middle_index]
+    
+    b = array[to_index]
+    if array[from_index] == required:
+        return from_index
+    if from_index == to_index:
+        return -1
     found = False
     while not found:
-        if middle == required:
+        # breakpoint()
+        if a < required < median:
+            left = find_in_partially_sorted_array(
+                array,
+                required,
+                from_index,
+                middle_index,
+            )
             found = True
-        breakpoint()
-        way = ''
-        if required < median:
-            go = 'right'
-        if median < required < middle:
-            right_index = (right_index - left_index) // 2
-            print('right_index: ', right_index)
-            middle = array[(right_index + left_index) // 2]
+            return left
+        elif median <= required < b:
+            right = find_in_partially_sorted_array(
+                array,
+                required,
+                middle_index,
+                to_index)
+            found = True
+            return right
         else:
-            left_index = (right_index + left_index) // 2
-            print('left_index: ', left_index)
-            middle = array[(right_index + left_index) // 2]
-
-    return middle
+            if required > a:  # 50% left
+                b = array[middle_index]
+                middle_index = (middle_index + from_index) // 2
+            elif required < a:  # 50% right
+                a = array[middle_index]
+                middle_index = (middle_index + to_index) // 2
+            median = array[middle_index]
+    # breakpoint()
+    return -1
 
 
 if __name__ == '__main__':
     with open('input.txt') as file:
-        n = file.readline()
+        n = int(file.readline())
         k = int(file.readline())
         arr = [int(x) for x in file.readline().split()]
-    print(find_in_partially_sorted_array(arr, k))
+    print(find_in_partially_sorted_array(arr, k, 0, n-1))

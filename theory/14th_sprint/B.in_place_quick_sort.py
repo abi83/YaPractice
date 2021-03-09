69010e7c1d7f6e916c8d7913af5fff83f7b3afb5
Abi88 <vladimir.kromm@yandex.ru>
2021-03-09 16:32:02 +0700

    14th sprint HW

--- theory/14th_sprint/B.in_place_quick_sort.py
+++ theory/14th_sprint/B.in_place_quick_sort.py
@@ -1,37 +1,51 @@
-import time
+def in_place_quick_sort(arr, left, right, pivot):
+    if right <= left:
+        return arr
+    left_origin = left
+    right_origin = right
+    double_pivot = False
+    left_pivot_pos, right_pivot_pos = left, right
+    while left <= right_pivot_pos:
+        if double_pivot:
+            if arr[left] < pivot:
+                arr[left_pivot_pos], arr[left] = arr[left], arr[left_pivot_pos]
+                left_pivot_pos += 1
+                left +=1
+            elif arr[left] > pivot:
+                arr[left], arr[right_pivot_pos] = arr[right_pivot_pos], arr[left]
+                right_pivot_pos -= 1
+            else:
+                left += 1
+        else:
+            if arr[left] >= pivot >= arr[right]:
+                if arr[left] == pivot == arr[right]:
+                    left_pivot_pos, right_pivot_pos = left, right
+                    double_pivot = True
+                arr[left], arr[right] = arr[right], arr[left]
+            if arr[left] < pivot:
+                left += 1
+            if arr[right] > pivot:
+                right -= 1
 
-
-def in_place_quick_sort(arr):
-    left = 0
-    right = len(arr) - 1
-    pivot = arr[0]
-    print(pivot)
-    pivots_count = 1
-    while left < right:
-        print('left: ', left, 'right: ', right, 'pivot: ', pivot)
-        print(*arr)
-        # time.sleep(0.5)
-        left_element = arr[left]
-        right_element = arr[right]
-        breakpoint()
-        if (arr[left] > pivot >= arr[right]):
-            arr[left], arr[right] = arr[right], arr[left]
-            continue
-        if arr[left] == pivot and arr[right] == pivot:
-            arr[left], arr[right-pivots_count] = arr[right-pivots_count], arr[left]
-            pivots_count += 1
-            right += 1
-            # left -=1
-            continue
-        if arr[left] < pivot:
-            left += 1
-        if arr[right] > pivot:
-            right -= 1
-
+    good_pivot = arr[left_pivot_pos - 1]
+    in_place_quick_sort(arr, left_origin, left_pivot_pos - 1, good_pivot)
+    try:
+        good_pivot = arr[right_pivot_pos+1]
+    except IndexError:
+        good_pivot = arr[-1]
+    in_place_quick_sort(arr, right_pivot_pos + 1, right_origin, good_pivot)
 
     return arr
 
 
 if __name__ == '__main__':
     with open('input.txt') as file:
-        print(in_place_quick_sort([int(x) for x in file.readline().split()]))
+        a = [int(x) for x in file.readline().split()]
+        print(
+            in_place_quick_sort(
+                a,
+                0,
+                len(a)-1,
+                7
+            )
+        )

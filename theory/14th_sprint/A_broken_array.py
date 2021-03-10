1a70ebf646d23bfd387dd9936bf9451f0bf5c48d
Abi88 <vladimir.kromm@yandex.ru>
2021-03-10 17:06:54 +0700

    broken array done

--- theory/14th_sprint/A_broken_array.py
+++ theory/14th_sprint/A_broken_array.py
@@ -1,45 +1,47 @@
-def find_in_partially_sorted_array(array, required, from_index, to_index):
-    a_index = from_index
-    middle_index = (from_index + to_index) // 2
-    median = array[middle_index]
-    
-    b_index = to_index
+def binary_search(array, required, from_index, to_index):
     if array[from_index] == required:
-        return from_index  # TODO: Ускорить тут
+        return from_index
     if from_index == to_index:
         return -1
-    found = False
-    while not found:
-        breakpoint()
-        if array[a_index] < required < median:
-            left = find_in_partially_sorted_array(
-                array,
-                required,
-                from_index,
-                middle_index,
-            )
-            found = True
-            return left
-        elif median <= required < array[b_index]:
-            right = find_in_partially_sorted_array(
-                array,
-                required,
-                middle_index+1,
-                to_index)
-            found = True
-            return right
-        else:
-            if required > array[a_index]:  # 50% left
-                b_index = middle_index
-                middle_index = (middle_index + from_index) // 2
-            elif required < array[a_index]:  # 50% right
-                a_index = middle_index
-                middle_index = (middle_index + to_index + 1) // 2
-            median = array[middle_index]
-            if a_index == b_index:
-                found = True
+    middle_index = (from_index + to_index) // 2
 
-    return -1
+    if array[from_index] < required <= array[middle_index]:
+        return binary_search(array, required, from_index, middle_index)
+    if array[middle_index+1] <= required <= array[to_index]:
+        return binary_search(array, required, middle_index+1, to_index)
+    return - 1
+
+
+def find_in_partially_sorted_array(array, required):
+    if len(array) == 0:
+        return -1
+    broken_index = find_broken_index(array, 0, len(array) - 1)
+    if broken_index == 0:
+        return binary_search(array, required, 0, len(array)-1)
+    elif array[0] <= required <= array[broken_index - 1]:
+        return binary_search(array, required, 0, broken_index-1)
+    elif array[broken_index] <= required <= array[len(array)-1]:
+        return binary_search(array, required, broken_index, len(array)-1)
+    else:
+        return -1
+
+
+def find_broken_index_cool(arr, from_index, to_index):
+    return arr.index(min(*arr))
+
+
+def find_broken_index(arr, from_index, to_index):
+    if to_index-from_index <= 1:
+        b = arr.index(min(arr[from_index:to_index+1]))
+        return b
+    middle_index = (from_index + to_index) // 2
+
+    if arr[middle_index] > arr[from_index]:
+        #левая половина стабильно растет, разрыва в ней нет
+        #отправляем рекурсию в правую половину
+        return find_broken_index(arr, middle_index, to_index)
+    else:
+        return find_broken_index(arr, from_index, middle_index)
 
 
 if __name__ == '__main__':
@@ -47,4 +49,7 @@ if __name__ == '__main__':
         n = int(file.readline())
         k = int(file.readline())
         arr = [int(x) for x in file.readline().split()]
-    print(find_in_partially_sorted_array(arr, k, 0, n-1))
+    print(find_in_partially_sorted_array(arr, k))
+    # print(find_broken_index(arr, 0, len(arr)-1))
+    # print(binary_search(arr, k, 4, 10))
+

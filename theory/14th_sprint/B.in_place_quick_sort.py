4956a59c9215114df66b7068ac0eabefc0008efe
Abi88 <vladimir.kromm@yandex.ru>
2021-03-16 18:47:56 +0700

    tl in 20 and 22

--- theory/14th_sprint/B.in_place_quick_sort.py
+++ theory/14th_sprint/B.in_place_quick_sort.py
@@ -1,74 +1,68 @@
 def in_place_quick_sort(
         array,
-        from_index: int = None,
-        to_index: int = None,
         compare_func=None,
 ) -> None:
     """
     Just one more version of in place quick sort algorithm. Array is
-    sorted on place, without extra memory usage. The sorting is ascending.
-    @param array: Any iterable of objects with fully defined comparison methods
+    sorted on place, without extra memory usage. Default sorting is ascending.
+    @param array: Any iterable of objects with defined comparison methods
     @param from_index: Index to start sorting
     @param to_index: Index to end sorting
-    @param compare_func: function of x,y returning any positive number if x>y,
-    negative number if x<y and 0 if x==y
+    @param compare_func: function of x,y returning True if x > y, esle False
     @return: None
     """
-    from_index = 0 if from_index is None else from_index
-    to_index = len(array) - 1 if to_index is None else to_index
-    if to_index <= from_index:
-        return
+    from_index = 0
+    to_index = len(array) - 1
     if compare_func is None:
-        compare_func = lambda x, y: x - y
-    pivot = array[to_index]
+        compare_func = lambda x, y: x > y
 
-    left = from_index
-    right_pivot_pos = to_index
-    left_pivot_pos = to_index
-    while left < left_pivot_pos:
-        comparison = compare_func(array[left], pivot)
-        if comparison < 0:
-            left += 1
-        elif comparison > 0:
-            array[left], array[right_pivot_pos] = array[right_pivot_pos], \
-                                                  array[left]
-            right_pivot_pos -= 1
-        elif comparison == 0:
-            left_pivot_pos -= 1
-            array[left], array[left_pivot_pos] = array[left_pivot_pos], array[
-                left]
+    def inner_in_place_quick_sort(array, from_index, to_index, compare_func):
+        if to_index <= from_index:
+            return
 
-    in_place_quick_sort(array, from_index, left_pivot_pos - 1, compare_func=compare_func)
-    in_place_quick_sort(array, right_pivot_pos + 1, to_index, compare_func=compare_func)
+        pivot = array[(to_index + from_index)//2]
+        left, right = from_index, to_index
+        while left < right:
+            lef_comparison = compare_func(pivot, array[left])
+            right_comparison = compare_func(array[right], pivot)
+            if not lef_comparison and not right_comparison:
+                array[left], array[right] = array[right], array[left]
+                continue
+            if lef_comparison:
+                left += 1
+            if right_comparison:
+                right -= 1
 
+        inner_in_place_quick_sort(array, from_index, left,
+                                  compare_func=compare_func)
+        inner_in_place_quick_sort(array, left+1, to_index,
+                                  compare_func=compare_func)
+
+    inner_in_place_quick_sort(array, from_index, to_index, compare_func)
     return array
 
 
 if __name__ == '__main__':
     from collections import namedtuple
-    Participant = namedtuple('Participant', ['name', 'tasks', 'penalty'])
+    Participant = namedtuple('Participant', ['tasks', 'penalty', 'name'])
 
     with open('input.txt') as in_file:
         participants_count = int(in_file.readline())
-        participants = []
-        for _ in range(participants_count):
-            data = in_file.readline().split()
-            participants.append(
-                Participant(
-                    name=data[0],
-                    tasks=int(data[1]),
-                    penalty=int(data[2])
-                ))
+        participants = [None] * participants_count
+        data = in_file.read().split('\n')
+        for index in range(participants_count):
+            line = data[index].split()
+            participants[index] = Participant(
+                name=line[0], tasks=int(line[1]), penalty=int(line[2]))
 
     def participants_compare(participant_x, participant_y):
-        if participant_x == participant_y:
-            return 0
-        if participant_x.tasks - participant_y.tasks != 0:
-            return participant_x.tasks - participant_y.tasks
-        if participant_x.penalty - participant_y.penalty != 0:
-            return participant_y.penalty - participant_x.penalty
-        return 1 if participant_y.name > participant_x.name else -1
-    in_place_quick_sort(participants, compare_func=participants_compare)
-
-    for participant_index in range(1, participants_count+1):
-        print(participants[-participant_index].name)
+        if participant_x.tasks != participant_y.tasks:
+            return participant_x.tasks < participant_y.tasks
+        if participant_x.penalty != participant_y.penalty:
+            return participant_x.penalty > participant_y.penalty
+        return participant_x.name > participant_y.name
+    in_place_quick_sort(participants,
+                        compare_func=participants_compare
+                        )
+    with open('output.txt', 'w') as out_file:
+        out_file.write('\n'.join(x.name for x in participants))

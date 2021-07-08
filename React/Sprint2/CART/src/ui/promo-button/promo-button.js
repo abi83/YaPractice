6da49614ce3ec22895132921adbb42926cb62bfb
Abi88 <vladimir.kromm@yandex.ru>
2021-07-08 08:47:01 +0200

    sprint2

---
+++ React/Sprint2/CART/src/ui/promo-button/promo-button.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './promo-button.module.css';
+import closeIcon from '../../images/close.svg';
+
+export const PromoButton = ({ children, extraClass, setPromo, setDiscount }) => {
+  const cancelPromo = () => {
+    setDiscount();
+    setPromo();
+    // TODO: обнулить название акции (promo)
+    // TODO: сбросить скидку (discount)
+  };
+
+  return (
+    <button type="button" className={`${styles.button} ${extraClass}`} onClick={cancelPromo}>
+      {children}
+      <img className={styles.close} src={closeIcon} alt="кнопка закрытия" />
+    </button>
+  );
+};

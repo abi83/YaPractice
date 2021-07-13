8a63b0a52523e6360d655b11d33b9adb098b6413
Abi88 <vladimir.kromm@yandex.ru>
2021-07-13 15:26:18 +0200

    1/2 done

--- React/Sprint2/CART/src/index.js
+++ React/Sprint2/CART/src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/app';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+
+import { createStore } from 'redux';
+import { rootReducer } from './services/reducers';
+
+const store = createStore(rootReducer);
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- React/Sprint2/CART/src/services/reducers/cart.js
+++ React/Sprint2/CART/src/services/reducers/cart.js
@@ -1,10 +1,25 @@
-const initialState = {
-  items: [],
+import {INCREASE_ITEM, CANCEL_PROMO, DECREASE_ITEM, DELETE_ITEM, TAB_SWITCH} from '../actions/cart'
+import { items, recommendedItems } from '../initialData'
 
-  recommendedItems: [],
+
+const initialState = {
+  items: items,
+  recommendedItems: items,
 
   promoCode: '',
   promoDiscount: null,
 
   currentTab: 'items'
 };
+
+
+export const cartReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREASE_ITEM: return state
+    case CANCEL_PROMO: return state
+    case DECREASE_ITEM: return state
+    case DELETE_ITEM: return state
+    case TAB_SWITCH: return state
+    default: return state
+  }
+}

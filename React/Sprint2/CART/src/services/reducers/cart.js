b9e42af87d52696efc9fb4e644fe59b7aa7b7a81
Abi88 <vladimir.kromm@yandex.ru>
2021-07-15 19:23:10 +0200

    1/5 done

--- React/Sprint2/CART/src/services/reducers/cart.js
+++ React/Sprint2/CART/src/services/reducers/cart.js
@@ -3,15 +3,20 @@ import {
   CANCEL_PROMO,
   DECREASE_ITEM,
   INCREASE_ITEM,
-  TAB_SWITCH
+  TAB_SWITCH,
+  GET_ITEMS_FAILED,
+  GET_ITEMS_REQUEST,
+  GET_ITEMS_SUCCESS
 } from '../actions/cart';
 import { recommendedItems, items } from '../initialData';
 
 const initialState = {
-  items,
+  items: [],
 
   recommendedItems,
-
+  itemsRequest: false,
+  itemsFailed: false,
+  
   promoCode: 'PROMOCODE',
   promoDiscount: 50,
 
@@ -50,6 +55,27 @@ export const cartReducer = (state = initialState, action) => {
         ...state,
         promoCode: '',
         promoDiscount: null
+      };
+    }
+    case GET_ITEMS_FAILED: {
+      return {
+        ...state,
+        itemsRequest: false,
+        itemsFailed: true,
+      };
+    }
+    case GET_ITEMS_REQUEST: {
+      return {
+        ...state,
+        itemsRequest: true,
+      };
+    }
+    case GET_ITEMS_SUCCESS: {
+      return {
+        ...state,
+        itemsRequest: false,
+        itemsFailed: false,
+        items: action.items,
       };
     }
     default: {
